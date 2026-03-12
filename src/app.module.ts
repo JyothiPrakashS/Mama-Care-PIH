5f15d3b119b01cd0705d65c2ef1c3dfbdb5c3b72
WILDCAT\Jyothi Prakash <jyocool34@gmail.com>
2026-03-12 19:09:36 +0530

    Solving backend server run issues - moved prisma module and service inside prisma folder, installed adapter and updated config based on the adapter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaService } from './common/prisma.service';
 import { AuthModule } from './modules/auth/auth.module';
+import { PrismaModule } from './common/prisma/prisma.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
     AuthModule,
   ],
-  providers: [PrismaService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
